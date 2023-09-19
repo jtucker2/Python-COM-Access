a3b9d245776f6a3d379dd872859a948ca4a08bba
Josh Tucker <joshuatucker648@gmail.com>
2023-09-19 14:33:24 +0100

    Added dumping single form

--- kraken.py
+++ kraken.py
@@ -10,19 +10,21 @@ currentData = project.Application.CurrentData
 
 exportPath = sys.argv[1]
 
+def dumpForm(formName):
+    project.DoCmd.OpenForm(formName)
+    project.Application.SaveAsText(2, formName, exportPath + "\export_" + formName + ".txt")
+    project.DoCmd.Close(2, formName)
+
 match sys.argv[2]:
     case "dump-forms":
         allForms = currentProject.AllForms
         formNames = []
         for i in range(allForms.Count):
             formNames.append(allForms.Item(i).Name)
-
         for formName in formNames:
-            project.DoCmd.OpenForm(formName)
-            form = project.Forms.Item(0)
-            project.Application.SaveAsText(2, formName, exportPath + "\export_" + formName + ".txt")
-            project.DoCmd.Close(2, formName)
-    case "test":
-        print("woop")
+            dumpForm(formName)
+
+    case "dump-form":
+        dumpForm(sys.argv[3])
 
 project.Application.Quit()
