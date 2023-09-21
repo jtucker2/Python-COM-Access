24ce54e9c1549451df4dc94d03333c4ad2547952
Josh Tucker <joshuatucker648@gmail.com>
2023-09-21 14:07:32 +0100

    Added dumping modules

--- kraken.py
+++ kraken.py
@@ -20,40 +20,61 @@ exportPath = sys.argv[2]
 
 def dumpForm(formName):
     project.DoCmd.OpenForm(formName)
-    project.Application.SaveAsText(2, formName, exportPath + "\export_" + formName + ".txt")
+    project.Application.SaveAsText(2, formName, exportPath + "\\" + formName + ".frm")
     project.DoCmd.Close(2, formName)
 
-def loadForm(formName):
-    project.Application.LoadFromText(2, formName, exportPath + "\export_" + formName + ".txt")
+def loadForm(formName):\
+    project.Application.LoadFromText(2, formName, exportPath + "\\" + formName + ".frm")
+
+def dumpModule(moduleName):
+    project.DoCmd.OpenModule(moduleName)
+    project.Application.SaveAsText(5, moduleName, exportPath + "\\" + moduleName + ".bas")
+    project.DoCmd.Close(5, moduleName)
 
 def extractFileName(fileName):
-    return fileName[7:].split(".")[0]
+    return fileName.split(".")[0]
+
+def dumpAllForms():
+    allForms = currentProject.AllForms
+    formNames = []
+    for i in range(allForms.Count):
+        formNames.append(allForms.Item(i).Name)
+
+    # TODO: this form wasn't working
+    formNames.remove("ProjectVariants")
+
+    for formName in formNames:
+        print(formName)
+        dumpForm(formName)
+
+def dumpAllModules():
+    allModules = currentProject.AllModules
+    moduleNames = []
+    for i in range(allModules.Count):
+        moduleNames.append(allModules.Item(i).Name)
+
+    for moduleName in moduleNames:
+        print(moduleName)
+        dumpModule(moduleName)
 
 match sys.argv[3]:
     case "dump-forms":
-        allForms = currentProject.AllForms
-        formNames = []
-        for i in range(allForms.Count):
-            formNames.append(allForms.Item(i).Name)
-
-        # TODO: this form wasn't working
-        formNames.remove("ProjectVariants")
-
-        for formName in formNames:
-            print(formName)
-            dumpForm(formName)
-
+        dumpAllForms()
     case "dump-form":
         dumpForm(sys.argv[4])
-    
     case "load-form":
         loadForm(sys.argv[4])
-
     case "load-forms":
         formNames = [extractFileName(name) for name in os.listdir(exportPath)]
 
         for formName in formNames:
             print(formName)
             loadForm(formName)
+    
+    case "dump-modules":
+        dumpAllModules()
+    case "dump-all":
+        dumpAllForms()
+        dumpAllModules()
 
 project.Application.Quit()
